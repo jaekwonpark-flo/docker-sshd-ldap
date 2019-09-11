1ad9e80563376cd197d24eff9e84803169d73a24
Ubuntu <ubuntu@ip-10-180-2-169.ap-northeast-2.compute.internal>
2019-09-11 09:52:35 +0000

    change userlist.sh and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM debian:jessie
-MAINTAINER jaekwon park <jaekwon.park@code-post.com>
+MAINTAINER jaekwon park <jaekwon.park@sk.com>
 
 # Configure apt
-RUN apt-get update \
-	&& DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends libnss-ldapd libpam-ldapd openssh-server ldap-utils nscd dialog fping rsyslog && \
-     apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends libnss-ldapd libpam-ldapd openssh-server ldap-utils nscd dialog fping rsyslog  python3-pip  curl && \
+    apt-get autoremove -y && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && \
+    pip3 install awscli --upgrade && mkdir -p /home/.aws/
 
 ADD entrypoint.sh record_ssh.sh /usr/local/bin/
 ADD ldap_auth.sh /ldap_auth/
 ADD userlist.sh /etc/profile.d/
 ADD protocols /etc/
+ADD aws.config /home/.aws/config
 
 RUN mkdir -p /var/run/sshd && chmod u+x /usr/local/bin/*.sh && chmod -R 755 /ldap_auth && chmod a+x /usr/local/bin/record_ssh.sh /etc/profile.d/userlist.sh
 
--- userlist.sh
+++ userlist.sh
@@ -5,6 +5,22 @@ MENU_LIST=$(mktemp /tmp/menu.list.XXX)
 SERVER_LIST=$(mktemp /tmp/server.XXX)
 BASTION_SERVER_IP=$(grep BASTION_SERVER_IP /BASTION_SERVER | awk '{print $2}')
 BASTION_SERVER_PORT=$(grep BASTION_SERVER_PORT /BASTION_SERVER | awk '{print $2}') 
+BASTION_ID=$(grep BASTION_ID /BASTION_SERVER | awk '{print $2}')
+REGION=$(grep REGION /BASTION_SERVER | awk '{print $2}')
+
+
+
+userid=$(id | awk '{print $1}' | awk -F\( '{print $2}' | sed -e s/\)//g)
+user_search=$(ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "LDAP_BASE" "(&(gidNumber=*)(uid=$userid))" gidNumber | grep gidNumber: | awk '{print $2}')
+group_name=$(ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "ou=group,LDAP_BASE" "(&(gidNumber=$user_search))" cn | grep cn: | awk '{print $2}')
+
+#ADMIN_DN="cn=admin,dc=example,dc=com"
+#ADMIN_DN_PASS="admin_pass"
+#LDAP_BASE="dc=example,dc=com"
+#REGION="ap-northeast-2"
+#user_search=$(ldapsearch -h localhost -D "$ADMIN_DN " -w $ADMIN_DN_PASS -x -b "$LDAP_BASE" "(&(gidNumber=*)(uid=$userid))" gidNumber | grep gidNumber: | awk '{print $2}')
+#group_name=$(ldapsearch -h localhost -D "$ADMIN_DN " -w $ADMIN_DN_PASS -x -b "ou=group,$LDAP_BASE" "(&(gidNumber=$user_search))" cn | grep cn: | awk '{print $2}')
+
 
 trap ctrl_c INT
 trap ctrl_c SIGINT
@@ -15,48 +31,32 @@ function ctrl_c() {
     exit
 }
 
-userid=$(id | awk '{print $1}' | awk -F\( '{print $2}' | sed -e s/\)//g)
 
-user_search=$(ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "LDAP_BASE" "(&(gidNumber=*)(uid=$userid))" gidNumber | grep gidNumber: | awk '{print $2}')
-
-group_name=$(ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "ou=group,LDAP_BASE" "(&(gidNumber=$user_search))" cn | grep cn: | awk '{print $2}')
-
-host_group=$(ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "ou=host,LDAP_BASE" "(&(cn=$group_name))" memberNisNetgroup | grep memberNisNetgroup: | awk '{print $2}')
-
-function get_server_list() {
-    if [[ -z $host_group ]]; then
-        ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "ou=host,LDAP_BASE" "(&(cn=$group_name))" nisNetgroupTriple | grep nisNetgroupTriple: | awk '{print $2}' | sed -e s/\(// | sed -e s/\)// >>$SERVER_LIST
+get_server_list () {
+    if [[ $group_name == "admin" ]]; then
+	aws ec2 describe-instances --query 'Reservations[*].Instances[*].{EAZ:Placement.AvailabilityZone,CInstance:PrivateIpAddress,Zssh:Tags[?Key==`ssh`]|[0].Value,AName:Tags[?Key==`Name`]|[0].Value,Buser:Tags[?Key==`ssh_user`]|[0].Value,Dinstanceid:InstanceId}' --output=text | grep -E "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])" | awk  -F'\t' '{  cmd="echo "$3" | md5sum | fold -w 6"; cmd | getline x; close(cmd) ; print $1"-"x","$2","$3","$4","$5","$6}' >> $SERVER_LIST
     else
-        for i in $host_group; do
-            ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "cn=$i,ou=host,LDAP_BASE" "(&(cn=*))" nisNetgroupTriple | grep nisNetgroupTriple: | awk '{print $2}' | sed -e s/\(// | sed -e s/\)// >>$SERVER_LIST
-        done
+	aws ec2 describe-instances --query 'Reservations[*].Instances[*].{EAZ:Placement.AvailabilityZone,CInstance:PrivateIpAddress,Zssh:Tags[?Key==`ssh`]|[0].Value,AName:Tags[?Key==`Name`]|[0].Value,Buser:Tags[?Key==`ssh_user`]|[0].Value,Dinstanceid:InstanceId}' --output=text | grep -E "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])" | grep -P '(^|\s)\K'$userid'(?=\s|$)' | awk  -F'\t' '{  cmd="echo "$3" | md5sum | fold -w 6"; cmd | getline x; close(cmd) ; print $1"-"x","$2","$3","$4","$5","$6}' >> $SERVER_LIST
     fi
 }
 
-function get_addtional_group_list() {
-    for i in $(ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "ou=group,LDAP_BASE" "(&(memberUid=$userid))" cn | grep cn: | awk '{print $2}'); do
-        ldapsearch -h ldap -D "ADMIN_DN " -w ADMIN_DN_PASS -x -b "ou=host,LDAP_BASE" "(&(cn=$i))" nisNetgroupTriple | grep nisNetgroupTriple: | awk '{print $2}' | sed -e s/\(// | sed -e s/\)// >>$SERVER_LIST
-    done
-
-}
-
 function set_menu_list() {
-    #	cat $SERVER_LIST | awk -F, '{print $1" "$3}' >>  $MENU_LIST
     count=0
     for i in $(cat $SERVER_LIST | awk -F, '{print $3}'); do
-        host_name=$(grep -w $i $SERVER_LIST | awk -F, '{print $1}')
-        if [[ $(fping -t 50 $i | grep -c "alive") -eq 1 ]]; then
-            echo "$host_name Alive" >>$MENU_LIST
+        host_name=$(grep -w $i $SERVER_LIST | awk -F, '{print $1}' | sed "s/ /-/g" )
+	
+        if [ $(fping -t 50 $i | grep -c "alive") -eq 1 ]; then
+            echo "$host_name Alive-[$(echo $i | awk -F. '{print $3"."$4}')]" >>$MENU_LIST
         else
-            echo "$host_name Down" >>$MENU_LIST
+            echo "$host_name Down-[$(echo $i | awk -F. '{print $3"."$4}')]" >>$MENU_LIST
         fi
         echo $count
-        count=$(expr $count + $((($RANDOM % 15))))
-        if [[ $count -gt "98" ]]; then
+	count=$(expr $count + $((($RANDOM % 3))))
+        if [ $count -gt "98" ]; then
             count=99
         fi
     done
-    if [ $group_name == "admin" ] && [ -n "$BASTION_SERVER_IP" ]; then
+    if [[ $group_name = "admin" ]] && [[ -n "$BASTION_SERVER_IP" ]]; then
         echo "Bastion_server Alive" >>$MENU_LIST
     fi
     echo 100
@@ -87,20 +87,19 @@ echo "############################################"
 }
 
 get_server_list
-get_addtional_group_list
 set_menu_list | $DIALOG --backtitle "SSH CONNECTOR" --title "Server Status Check" --gauge "Find Alive Servers..." 6 80 0
 
 server_alive=$(grep -c Alive $MENU_LIST)
 server_down=$(grep -c Down $MENU_LIST)
 
-if [[ $(id -u) -ne 0 ]]; then
+if [ $(id -u) -ne 0 ]; then
     menu=$(cat $MENU_LIST)
 
-    if [[ ! -f /sshd_key/$userid ]]; then
+    if [ ! -f /sshd_key/$userid ]; then
         while [ -z $ssh_pass ]; do
             ssh_pass=$($DIALOG --title "Password for sshkey file" --cancel-label "Exit" \
                 --clear --insecure --passwordbox "Enter your ssh-key File  Password(not ssh id password)" 20 80 3>&1 1>&2 2>&3 3>&-)
-            if [[ $? -ne 0 ]]; then
+            if [ $? -ne 0 ]; then
                 clear
                 echo "Exit from User"
                 exit 0
@@ -109,7 +108,7 @@ if [[ $(id -u) -ne 0 ]]; then
 
             ssh_pass_verify=$($DIALOG --title "Password for sshkey file" --cancel-label "Exit" \
                 --clear --insecure --passwordbox "Enter your ssh-key File  Password Again" 20 80 3>&1 1>&2 2>&3 3>&-)
-            if [[ $? -eq 0 ]]; then
+            if [ $? -eq 0 ]; then
                 if [ $ssh_pass == $ssh_pass_verify ] && [ $ssh_pass != "" ]; then
                     if ! ssh-keygen -f /sshd_key/$userid -P $ssh_pass -q; then
                         clear
@@ -117,7 +116,7 @@ if [[ $(id -u) -ne 0 ]]; then
                         exit
                     fi
                 else
-                    if [[ -z $ssh_pass ]]; then
+                    if [ -z $ssh_pass ]; then
                         $DIALOG --title "Password for sshkey file" --clear --msgbox "Password is empty" 20 80
                     else
                         $DIALOG --title "Password for sshkey file" --clear --msgbox "Password doesn't match" 20 80
@@ -139,19 +138,23 @@ if [[ $(id -u) -ne 0 ]]; then
             --title "SSH Server List" --clear \
             --menu "[ID:$userid]    [GROUP:$group_name] \n $server_alive Server is Alive. $server_down Server is Down. \n [Select Server To Connect] " 30 80 22 $menu 3>&1 1>&2 2>&3 3>&-)
 
-        if [[ $? -eq 0 ]]; then
+        if [ $? -eq 0 ]; then
             clear
             host_ip=$(grep "^$connect_host," $SERVER_LIST | awk -F, '{print $3}')
             user_id=$(grep "^$connect_host," $SERVER_LIST | awk -F, '{print $2}')
-            if [[ -z $user_id ]]; then
+            instance_id=$(grep "^$connect_host," $SERVER_LIST | awk -F, '{print $4}')
+            aws_az=$(grep "^$connect_host," $SERVER_LIST | awk -F, '{print $5}')
+            if [ -z $user_id ]; then
                 user_id=$userid
             fi
-            if [[ $connect_host != "Bastion_server" ]]; then
-                if [[ $(fping -t 50 $host_ip | grep -c "alive") -eq 1 ]]; then
+            if [ $connect_host != "Bastion_server" ]; then
+                if [ $(fping -t 50 $host_ip | grep -c "alive") -eq 1 ]; then
                     print_connect_message $connect_host
                     logger -t [BASTION] -i -p authpriv.info connect to server $user_id@$host_ip
                     rm -rf $MENU_LIST $SERVER_LIST
-                    ssh -q -X -i /sshd_key/$userid -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user_id@$host_ip && \
+		    aws ec2-instance-connect send-ssh-public-key --region $REGION --instance-id $instance_id  --availability-zone $aws_az --instance-os-user $user_id --ssh-public-key file:///sshd_key/$userid.pub
+	       	    ssh -q -X -i /sshd_key/$userid -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user_id@$host_ip && \
+#		    mssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@$instance_id
                     logger -t [BASTION] -i -p authpriv.info logout to server $user_id@$host_ip && \
                     exit
                 else
@@ -160,6 +163,7 @@ if [[ $(id -u) -ne 0 ]]; then
                     echo "############################################"
                     echo "#              Server is Down              #" 
                     echo "############################################"
+		    echo $host_ip 
                     exit
                 fi
                 exception_exit
@@ -167,7 +171,13 @@ if [[ $(id -u) -ne 0 ]]; then
                 print_connect_message $connect_host
                 logger -t [BASTION] -i -p authpriv.info connect to server $connect_host
                 rm -rf $MENU_LIST $SERVER_LIST
-                ssh -q -p $BASTION_SERVER_PORT -i /sshd_key/$userid -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user_id@$BASTION_SERVER_IP && \
+		instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+		aws_az=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+		host_ip=$(curl -s  http://169.254.169.254/latest/meta-data/local-ipv4)
+		user_id=$(echo $BASTION_ID)
+		aws ec2-instance-connect send-ssh-public-key --region $REGION --instance-id $instance_id  --availability-zone $aws_az --instance-os-user $user_id --ssh-public-key file:///sshd_key/$userid.pub
+	       	ssh -q -X -p $BASTION_SERVER_PORT -i /sshd_key/$userid -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user_id@$host_ip && \
+                #ssh -q -p $BASTION_SERVER_PORT -i /sshd_key/$userid -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user_id@$BASTION_SERVER_IP && \
                 logger -t [BASTION] -i -p authpriv.info logout to server $connect_host && \
                 exit
                 exception_exit
